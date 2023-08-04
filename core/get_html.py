4fd88f0e504f717072145c18399d6a7821f6d319
Yurii Plets <de.jure.software@gmail.com>
2023-08-04 12:15:00 +0300

    Update get_html.py

--- core/get_html.py
+++ core/get_html.py
@@ -4,24 +4,25 @@ import time
 import requests
 
 
-def get_html(url: str) -> str:
+def get_html(url: str, timeout: int = 10) -> str:
     """Get HTML from page URL.
 
     Args:
         url (str): URL of the page.
+        timeout (int, optional): Timeout for the request in seconds. Defaults to 10.
 
     Returns:
         str: HTML of the page.
     """
     while True:
         try:
-            response = requests.get(url)
+            response = requests.get(url, timeout=timeout)
             if response.status_code == 200:
                 return response.text
 
             print(f'[{response.status_code}]: {url}')
             time.sleep(60)
 
-        except Exception as err: # pylint: disable=broad-except
+        except Exception as err:  # pylint: disable=broad-except
             print(f'[{err}]: {url}')
             time.sleep(60)
